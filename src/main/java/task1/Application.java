a4fdf3fbd8cc7d2e18bd924434e701b92e8e65db
huihuijiangqiang <59404531+huihuijiangqiang@users.noreply.github.com>
2020-10-28 10:56:14 +0800

    -

--- src/main/java/task1/Application.java
+++ src/main/java/task1/Application.java
@@ -1,3 +1,5 @@
+package task1;
+
 public class Application {
     public static void main(String[] args) {
         //摄像头开关
--- src/main/java/task1/Command.java
+++ src/main/java/task1/Command.java
@@ -1,3 +1,5 @@
+package task1;
+
 public interface Command {
     public abstract void OpenSwitch();
     public abstract void CloseSwitch();
